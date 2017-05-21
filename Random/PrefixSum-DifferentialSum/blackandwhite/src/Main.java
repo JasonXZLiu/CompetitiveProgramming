cb330da906f2300cd976b5fc68b487bfb7410ed4
JasonXZLiu <jason.XZ.liu@gmail.com>
2017-05-21 13:00:32 -0400

    finished black and white
    switched the 2d int array into a 2d boolean array because the 2d int
    array took too much memory

--- Random/PrefixSum-DifferentialSum/blackandwhite/src/Main.java
+++ Random/PrefixSum-DifferentialSum/blackandwhite/src/Main.java
@@ -5,32 +5,51 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner s = new Scanner(System.in);
+        boolean debug = true;
         int size = s.nextInt();
-        byte[][] board = new byte[size][size];
+        boolean[][] board = new boolean[size][size];
         int c = s.nextInt();
         int r1;
         int c1;
-        int r2;
         int c2;
         for (int i = 0; i < c; i++) {
             r1 = s.nextInt();
             c1 = s.nextInt();
             c2 = s.nextInt() + c1 - 1;
-            r2 = s.nextInt() + r1 - 1;
             // update differential array
-            if (c2 == size - 1) {
-                board[r1][c1] += 1;
-                board[r1][c2] -= 1;
-                board[r2][c1] -= 1;
-                board[r2][c2] += 1;
+            if (s.nextInt() + r1 - 1 == size - 1 && c2 == size - 1) {
+                board[r1][c1] = !board[r1][c1];
+            } else if (s.nextInt() + r1 - 1 == size - 1) {
+                board[r1][c1] = !board[r1][c1];
+                board[r1][c2+1] = !board[r1][c2+1];
+            } else if (c2 == size - 1) {
+                board[r1][c1] = !board[r1][c1];
+                board[s.nextInt() + r1 - 1+1][c1] = !board[s.nextInt() + r1 - 1+1][c1];
             } else {
-                board[r1][c1] += 1;
-                board[r1][c2+1] -= 1;
-                board[r2+1][c1] -= 1;
-                board[r2+1][c2+1] += 1;
+//                if (debug) {
+//                    System.out.println(r1);
+//                    System.out.println(c1);
+//                    System.out.println(s.nextInt() + r1 - 1);
+//                    System.out.println(c2);
+//                }
+                board[r1][c1] = !board[r1][c1];
+                board[s.nextInt() + r1 - 1+1][c1] = !board[s.nextInt() + r1 - 1+1][c1];
+                board[r1][c2+1] = !board[r1][c2+1];
+                board[s.nextInt() + r1 - 1+1][c2+1] = !board[s.nextInt() + r1 - 1+1][c2+1];
             }
         }
-        byte[][] sum = new byte[size][size];
+
+//        if (debug) {
+//            for (int i = 0; i < size; i++) {
+//                for (int j = 0; j < size; j++) {
+//                    System.out.print(board[i][j] + " ");
+//                }
+//                System.out.println("");
+//            }
+//        System.out.println("Sum");
+//        }
+
+        boolean[][] sum = new boolean[size][size];
         int count = 0;
         for (int i = 0; i < size; i ++) {
             for (int j = 0; j < size; j++) {
@@ -38,18 +57,26 @@ public class Main {
                     if (j == 0) {
                         sum[0][0] = board[0][0];
                     } else {
-                        sum[0][j] = (byte) (board[0][j] + sum[0][j-1]);
+                        sum[0][j] = board[0][j] ^ sum[0][j-1];
                     }
                 } else if (j == 0) {
-                    sum[i][0] = (byte) (board[i][0] + sum[i - 1][0]);
+                    sum[i][0] = board[i][0] ^ sum[i - 1][0];
                 } else {
-                    sum[i][j] = (byte) (sum[i-1][j] + sum[i][j-1] + board[i][j] - sum[i-1][j-1]);
+                    sum[i][j] = sum[i-1][j] ^ sum[i][j-1] ^ board[i][j] ^ sum[i-1][j-1];
                 }
-                if (sum[i][j] % 2 == 1) {
+                if (sum[i][j]) {
                     count += 1;
                 }
             }
         }
+//        if (debug) {
+//            for (int i = 0; i < size; i++) {
+//                for (int j = 0; j < size; j++) {
+//                    System.out.print(sum[i][j] + " ");
+//                }
+//                System.out.println("");
+//            }
+//        }
         System.out.println(count);
     }
 }
